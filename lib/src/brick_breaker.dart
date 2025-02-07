b5a68fb4a3124957be9be2f526a6d6def561de80
Tu Nombre <jorcabdes@alu.edu.gva.es>
2025-02-07 18:10:36 +0100

    first commit

---
+++ lib/src/brick_breaker.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'dart:math' as math; // Add this import
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'components/components.dart';
+import 'config.dart';
+class BrickBreaker extends FlameGame with HasCollisionDetection {
+  BrickBreaker()
+    : super(
+      camera: CameraComponent.withFixedResolution(
+      width: gameWidth,
+      height: gameHeight,
+      ),
+    );
+  final rand = math.Random(); 
+  double get width => size.x;
+  double get height => size.y;
+  @override
+  FutureOr<void> onLoad() async {
+    super.onLoad();
+    camera.viewfinder.anchor = Anchor.topLeft;
+    world.add(PlayArea());
+    world.add(Ball( 
+        radius: ballRadius,
+        position: size / 2,
+        velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2)
+        .normalized()
+        ..scale(height / 4)));
+    debugMode = true; 
+  }
+}
