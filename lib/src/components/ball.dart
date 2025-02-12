226bf1e06be7ef4f0d52cc39b40883d29311c1bc
Tu Nombre <jorcabdes@alu.edu.gva.es>
2025-02-12 18:32:48 +0100

    Add existing file

--- lib/src/components/ball.dart
+++ lib/src/components/ball.dart
@@ -1,7 +1,9 @@
 import 'package:flame/collisions.dart'; 
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
+import 'package:flame/effects.dart';
 import '../brick_breaker.dart'; 
+import 'bat.dart'; 
 import 'play_area.dart'; 
 class Ball extends CircleComponent with CollisionCallbacks, HasGameReference<BrickBreaker> {
   
@@ -35,8 +37,14 @@ class Ball extends CircleComponent with CollisionCallbacks, HasGameReference<Bri
       } else if (intersectionPoints.first.x >= game.width) {
         velocity.x = -velocity.x;
       } else if (intersectionPoints.first.y >= game.height) {
-        removeFromParent();
+        add(RemoveEffect( // Modify from here...
+          delay: 0.35,
+        ));
       }
+    } else if (other is Bat) {
+      velocity.y = -velocity.y;
+      velocity.x = velocity.x +
+        (position.x - other.position.x) / other.size.x * game.width * 0.3;
     } else {
     debugPrint('collision with $other');
     }
